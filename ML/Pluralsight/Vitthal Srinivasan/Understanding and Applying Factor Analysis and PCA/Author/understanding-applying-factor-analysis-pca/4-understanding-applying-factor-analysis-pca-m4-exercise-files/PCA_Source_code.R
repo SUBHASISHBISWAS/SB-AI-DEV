1de12eb740b1d164478f7df37a18877a7d89baad
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-04-02 17:53:31 +0530

    Understanding and Applying Factor Analysis and PCA

---
+++ ML/Pluralsight/Vitthal Srinivasan/Understanding and Applying Factor Analysis and PCA/Author/understanding-applying-factor-analysis-pca/4-understanding-applying-factor-analysis-pca-m4-exercise-files/PCA_Source_code.R
@@ -0,0 +1,46 @@
+
+
+combinedFile <- 'C:/Users/admin/Downloads/Factor_Analysis/Returns.csv'
+prices <- read.table(combinedFile,header = TRUE,sep = ",")
+prices[,c("Date")] <- as.Date(prices[,c("Date")],"%m/%d/%Y")
+
+prices <- prices[order(prices$Date, decreasing = TRUE),]
+
+returns = prices
+
+returns[-nrow(returns),-1] <- returns[-nrow(returns),-1]/returns[-1,-1]-1
+returns <- returns[-nrow(returns),]  
+yVars <- returns[c("AAPL","XOM","CVX","VLO","S.P","FVX")]
+
+
+eig <- eigen(cov(scale(yVars)))
+
+eValues <- eig$values
+eVectors <- eig$vectors
+
+plot(eValues,type="b")
+eVectors %*% t(eVectors) 
+
+pca1 <- as.matrix(scale(yVars)) %*% eVectors[,1]
+pca2 <- as.matrix(scale(yVars)) %*% eVectors[,2]
+pca3 <- as.matrix(scale(yVars)) %*% eVectors[,3]
+
+pca <- data.frame(pca1,pca2,pca3)
+cor(pca)
+
+pcaModel <- lm(returns$GOOG ~ pca1 + pca2 + pca3)
+summary(pcaModel)
+
+plot(pcaModel$resid,returns$GOOG)
+acf(pcaModel$resid)
+
+
+multipleModel <- lm(returns$GOOG ~ returns$S.P + returns$FVX + returns$AAPL)
+summary(multipleModel)
+plot(multipleModel$resid,returns$GOOG)
+acf(multipleModel$resid)
+
+principal(cov(yVars),nfactors = 3,rotate="none")
+
+
+
---
+++ ML/Pluralsight/Vitthal Srinivasan/Understanding and Applying Factor Analysis and PCA/Author/understanding-applying-factor-analysis-pca/5-understanding-applying-factor-analysis-pca-m5-exercise-files/Factor_Analysis_Pluralsight.py
@@ -0,0 +1,70 @@
+
+import pandas as pd 
+import numpy as np
+
+
+combinedFile = 'C:/Users/admin/Downloads/Factor_Analysis/Returns.csv'
+
+
+
+data=pd.read_csv(combinedFile ,sep=",")
+data.head()
+
+data['Date'] = pd.to_datetime(data['Date'], format='%m/%d/%Y')
+data = data.sort_values(['Date'], ascending=[True])
+
+returns = data[[key for key in dict(data.dtypes) if dict(data.dtypes)[key] in ['float64', 'int64']]].pct_change()
+returns = returns[1:]
+
+
+yVars = returns[["AAPL","XOM","CVX","VLO","S&P","FVX"]]
+
+stdYVars = (yVars - yVars .mean()) / (yVars .std())
+
+stdReturns = (returns - returns .mean()) / (returns .std())
+
+yVars .cov()
+yVars .corr()
+
+eig_val, eig_vec = np.linalg.eig(stdYVars.cov())
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eig_pairs = [(np.abs(eig_val[i]), eig_vec[:,i]) for i in range(len(eig_val))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eig_pairs.sort(key=lambda x: x[0], reverse=True)
+
+# Visually confirm that the list is correctly sorted by decreasing eigenvalues
+for i in eig_pairs:
+    print(i[0])
+
+
+eVector1 = eig_pairs[0][1]
+eVector2 = eig_pairs[1][1]
+eVector3 = eig_pairs[2][1]
+
+
+pca1 = np.dot(stdYVars,eVector1.reshape(-1,1)).reshape(1,-1)
+pca2 = np.dot(stdYVars,eVector2.reshape(-1,1)).reshape(1,-1)
+pca3 = np.dot(stdYVars,eVector3.reshape(-1,1)).reshape(1,-1)
+
+
+xData = np.array(zip(pca1.T,pca2.T,pca3.T)).reshape(-1,3)
+yData = stdReturns["GOOG"]
+
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+goodGoogModel = linear_model.LinearRegression()
+goodGoogModel .fit(xData,yData)
+goodGoogModel .score(xData,yData)
+
+
+
+model = sm.OLS(yData, xData)
+results = model.fit()
+print(results.summary())
+
+
