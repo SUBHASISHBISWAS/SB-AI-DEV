2fff3ba46867b848cfae8635b8cf22b740539fd6
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-14 13:57:15 +0530

    Direct Drawing on Images with a mouse

--- ML/Udemy/jose portilla/Python for Computer Vision with OpenCV and Deep Learning/SB/6_Direct_Drwaing_On_Images_With_Mouse.py
+++ ML/Udemy/jose portilla/Python for Computer Vision with OpenCV and Deep Learning/SB/6_Direct_Drwaing_On_Images_With_Mouse.py
@@ -1,11 +1,24 @@
 import cv2
 import numpy as np
+# Create a function based on a CV2 Event (Left button  DOUBLE click)
+def draw_circle(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img,(x,y),100,(0,255,0),-1)
 
-img=np.zeros((512,512,3),np.int8)
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+# This names the window so we can reference it 
+cv2.namedWindow(winname='my_drawing')
+# Connects the mouse button to our callback function
+cv2.setMouseCallback('my_drawing',draw_circle)
 
-while True:
-    cv2.imshow('image',img)
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+while True: #Runs forever until we break with Esc key on keyboard
+    # Shows the image window
+    cv2.imshow('my_drawing',img)
+    # EXPLANATION FOR THIS LINE OF CODE:
+    # https://stackoverflow.com/questions/35372700/whats-0xff-for-in-cv2-waitkey1/39201163
+    if cv2.waitKey(20) & 0xFF == 27:
         break
+# Once script is done, its usually good practice to call this line
+# It closes all windows (just in case you have multiple windows called)
 cv2.destroyAllWindows()
-cv2.waitKey(1)
