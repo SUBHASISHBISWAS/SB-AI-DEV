f81dab55b25df536ce1a7384f356dd545f2b07f5
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2024-09-09 15:26:43 +0530

    Ollama with Langchain

---
+++ DL/Udemy/Generative-AI/Krish Naik/SUBHASISH BISWAS/Langchain Projects/1-Langchain/1.2-ollama/app.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+
+
+
+load_dotenv()
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With Gemma Model")
+input_text=st.text_input("What question you have in mind?")
+
+## Ollama Lamma Model
+llm = Ollama(model="llama3.1")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
