59a450361110adddbf69a9b1180c69cb05770290
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2025-09-22 15:11:26 +0530

    Add initial LangChain course project files
    Introduces multiple LangChain-based project directories for Eden Marco and Krish Naik Udemy courses, including .gitignore, main scripts, configuration files, and supporting modules for ReAct Algorithm, search agent, chat with PDF, code interpreter, and hello world projects.

---
+++ GEN-AI/Udemy/Eden Marco/LangChain/Author/langchain-course-project-ReAct-search-agent/main.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-5")
+react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
+)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
+
+
+def main():
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
